d7b4e6b23a021f5d729ca2cbc9ef1411600498a1
Zasenko <dmitry.zasenko@gmail.com>
2022-12-29 17:54:06 +0100

    add empty project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Dmitry Zasenko on 29.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
