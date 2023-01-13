d352cba3d48f78a005ee2dde003e28e8061e0ff4
Zasenko <dmitry.zasenko@gmail.com>
2023-01-13 20:40:29 +0100

    add Router and ModulBuilder

---
+++ WeatherApp/WeatherApp/AbstractRouterProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  AbstractRouterProtocol.swift
+//  WeatherApp
+//
+//  Created by Dmitry Zasenko on 13.01.23.
+//
+
+import Foundation
---
+++ WeatherApp/WeatherApp/Flow/TabBar/View/TabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  WeatherApp
+//
+//  Created by Dmitry Zasenko on 13.01.23.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
